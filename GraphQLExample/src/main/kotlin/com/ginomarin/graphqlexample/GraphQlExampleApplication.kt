2f1d8daae12b09cb8479475ba4ec5e87acf82276
Gino <GMarin@wfscorp.com>
2022-07-17 05:22:23 -0600

    Initializing Springboot Project

---
+++ GraphQLExample/src/main/kotlin/com/ginomarin/graphqlexample/GraphQlExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.ginomarin.graphqlexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphQlExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphQlExampleApplication>(*args)
+}
---
+++ GraphQLExample/src/test/kotlin/com/ginomarin/graphqlexample/GraphQlExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ginomarin.graphqlexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphQlExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
